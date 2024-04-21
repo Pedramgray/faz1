57334131083252edc49b887181c1f30b3db32fc9
pedramgray <pedramgray2106@gmail.com>
2024-04-21 09:48:42 +0330

    second commit

---
+++ splashpage.cpp
@@ -0,0 +1,25 @@
+#include "splashpage.h"
+#include "ui_splashpage.h"
+#include "signup.h"
+
+splashpage::splashpage(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::splashpage)
+{
+    ui->setupUi(this);
+}
+
+splashpage::~splashpage()
+{
+    delete ui;
+}
+
+
+void splashpage::on_pushButton_clicked()
+{
+    signup *w2=new signup;
+    w2->show();
+    this->hide();
+
+}
+
